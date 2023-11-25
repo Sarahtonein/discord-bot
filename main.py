e72dde93e8d37dda4dfc9825470a60150be451f5
Ekansh <goenka.ekansh@gmail.com>
2023-11-25 08:55:34 +0800

    added ping, pong command and optimized the dxy command

--- main.py
+++ main.py
@@ -20,15 +20,10 @@ async def on_ready():
 @bot.event
 async def on_message(message):
     if message.content.startswith("dxy"):
-        random_number = random.randint(0, 3)
-        if random_number == 0:
-            await message.channel.send(f'I"d like some freedom fries with my DXY')
-        elif random_number == 1:
-            await message.channel.send(f'Cleanest shirt in the dirty pile they say')
-        elif random_number == 2:
-            await message.channel.send(f'🦅🦅🦅:flag_us::flag_us:')
-        else:
-            await message.channel.send(f'DUMP ET')
+        # makes it so you dont have to do extra work to choose a random message, random.choice() picks 1 element from a list at random
+        messages = [f'🦅🦅🦅:flag_us::flag_us:', f'Cleanest shirt in the dirty pile they say', f'I"d like some freedom fries with my DXY', f'DUMP ET']
+        await message.channel.send(random.choice(messages))
+
 
     if message.content.startswith("morning"):
         await message.channel.send(f'Good morning sunshine, the earth says Hello :white_sun_cloud:')
@@ -43,6 +38,9 @@ async def on_message(message):
 
     if "coffee" in message.content and not message.author.bot:
         await message.channel.send(f'make me a cup too, bitch')
+        
+    if message.content.startswith("ping"):
+        await message.channel.send(f'pong')
 
 ##@bot.command
 ##async def 
