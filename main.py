a47e08282acb8c3bbc3c575cc98b3b3cc0055d46
Sarahtonein <joemoe1899@gmail.com>
2023-11-25 18:11:16 +1000

    update commands, lowercase string before comparison

--- main.py
+++ main.py
@@ -33,29 +33,39 @@ async def on_message(message):
     cooldown_key = f"{message.author.id}-{message.channel.id}"
     if cooldown_key in cooldowns and datetime.utcnow() < cooldowns[cooldown_key]:
         return
+    
+    content_lower = message.content.lower()
 
-    if "dxy" in message.content and not message.author.bot:
+    if "dxy" in content_lower and not message.author.bot:
         set_cooldown(message.author.id, message.channel.id)
         messages = [f'🦅🦅🦅:flag_us::flag_us:', f'Cleanest shirt in the dirty pile they say', f'I"d like some freedom fries with my DXY', f'DUMP ET']
         await message.channel.send(random.choice(messages))
 
-    elif message.content.startswith("morning"):
+    elif "morning" in content_lower and not message.author.bot:
         set_cooldown(message.author.id, message.channel.id)
         await message.channel.send(f'Good morning sunshine, the earth says Hello :white_sun_cloud:')
 
-    elif "bot" in message.content and not message.author.bot:
+    elif "bot" in content_lower and not message.author.bot:
         set_cooldown(message.author.id, message.channel.id)
         messages = [f'Who are you calling a bot, bot?', f'You rang?']
         await message.channel.send(random.choice(messages))
 
-    elif "food" in message.content and not message.author.bot:
+    elif "food" in content_lower and not message.author.bot:
         set_cooldown(message.author.id, message.channel.id)
         await message.channel.send(f'Go make yourself some food then, I ain`t doing it')
 
-    elif "coffee" in message.content and not message.author.bot:
+    elif "coffee" in content_lower and not message.author.bot:
         set_cooldown(message.author.id, message.channel.id)
         await message.channel.send(f'make me a cup too, bitch')
-        
+
+    elif "sandwich" in content_lower and not message.author.bot:
+        set_cooldown(message.author.id, message.channel.id)
+        await message.channel.send(f'Get me some of that Wi-fi bread!')
+
+    elif ("bitcoin" in content_lower or "btc" in content_lower) and not message.author.bot:
+        set_cooldown(message.author.id, message.channel.id)
+        await message.add_reaction("🚀")
+                
     elif message.content.startswith("ping"):
         set_cooldown(message.author.id, message.channel.id)
         await message.channel.send(f'pong')
